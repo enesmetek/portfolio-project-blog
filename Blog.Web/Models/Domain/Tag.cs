3be748a8b368ea8a5e6e2b7164c367b5fb70992e
enesmetek <emkafali@gmail.com>
2023-09-19 21:52:31 +0300

    Add project files.

---
+++ Blog.Web/Models/Domain/Tag.cs
@@ -0,0 +1,14 @@
+﻿namespace Blog.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        //Navigation Properties
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
