6dfa3cfbec29a07890732969f0610cda7e3caae9
enesmetek <emkafali@gmail.com>
2023-09-23 14:17:10 +0300

    Add Blog Post Page added.

---
+++ Blog.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,78 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Blog.Web.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+        private readonly IBlogPostRepository _blogPostRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository, IBlogPostRepository blogPostRepository)
+        {
+            _tagRepository = tagRepository;
+            _blogPostRepository = blogPostRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var tags = await _tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.ID.ToString()})
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            //Map View Model to Domain Model
+            var blogPost = new BlogPost
+            {
+                Heading = addBlogPostRequest.Heading,
+                PageTitle = addBlogPostRequest.PageTitle,
+                Content = addBlogPostRequest.Content,
+                ShortDescription = addBlogPostRequest.ShortDescription,
+                FeaturedImageUrl = addBlogPostRequest.FeaturedImageUrl,
+                UrlHandle = addBlogPostRequest.UrlHandle,
+                PublishedDate = addBlogPostRequest.PublishedDate,
+                Author = addBlogPostRequest.Author,
+                Visible = addBlogPostRequest.Visible
+            };
+
+            //Map Tags from Selected Tags
+            var selectedTags = new List<Tag>();
+            foreach(var selectedTagID in addBlogPostRequest.SelectedTags)
+            {
+                var selectedTagIDAsGuid = Guid.Parse(selectedTagID);
+                var existingTag = await _tagRepository.GetAsync(selectedTagIDAsGuid);
+
+                if (existingTag != null)
+                {
+                    selectedTags.Add(existingTag);
+                }
+            }
+
+            //Map tags back to Domain Model
+            blogPost.Tags = selectedTags;
+
+            await _blogPostRepository.AddAsync(blogPost);
+            return RedirectToAction("Add");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Blog.Web/Repositories/Concrete/BlogPostRepository.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Repositories.Concrete
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly BlogDbContext _blogDbContext;
+
+        public BlogPostRepository(BlogDbContext blogDbContext)
+        {
+            _blogDbContext = blogDbContext;
+        }
+
+        public async Task<BlogPost> AddAsync(BlogPost blogPost)
+        {
+            await _blogDbContext.AddAsync(blogPost);
+            await _blogDbContext.SaveChangesAsync();
+            return blogPost;
+        }
+
+        public Task<BlogPost?> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<BlogPost>> GetAllAsync()
+        {
+            var blogPosts = await _blogDbContext.BlogPosts.ToListAsync();
+            return blogPosts;
+        }
+
+        public Task<BlogPost?> GetAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BlogPost?> UpdateAsync(BlogPost blogPost)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
